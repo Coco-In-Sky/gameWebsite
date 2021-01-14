633844fdf36931a7a19f9f4f6a9ecb8df7329c57
Ziyang Luo <ziyangluo@luoziyang.local>
2021-01-14 00:06:50 -0500

    [first change with react]

--- frontend/src/components/weToGame.js
+++ frontend/src/components/weToGame.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import welcomeComponent from './welcomeComponent.js'
+import HeaderComponent from './HeaderComponent.js'
+import FooterComponent from './FooterComponent.js'
+import ErrorComponent from './ErrorComponent.js'
+
 
 class weToGame extends Component{
     render(){
@@ -8,15 +12,7 @@ class weToGame extends Component{
             <div className="weToGame">
                <Router>
                    <>
-                       <HeaderComponent/>
                        <Switch>
-                           // <Route path="/" exact component={LoginComponent}/>
-                           // <Route path="/login" component={LoginComponent}/>
-                           // <AuthenticatedRoute path="/welcome/:name" component={WelcomeComponent}/>
-                           // <AuthenticatedRoute path="/todos/:id" component={TodoComponent}/>
-                           // <AuthenticatedRoute path="/todos" component={ListTodosComponent}/>
-                           // <AuthenticatedRoute path="/logout" component={LogoutComponent}/>
-
                            <Route component={ErrorComponent}/>
                        </Switch>
                        <FooterComponent/>
